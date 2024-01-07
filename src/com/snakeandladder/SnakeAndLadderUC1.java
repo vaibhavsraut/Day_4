02be0d5c9c5222085c2fa8a7b6699e50718c94af
vaibhavsraut <vaibhav.raut851@gmail.com>
2024-01-07 13:13:12 +0530

    UC1: Snake and Ladder game with single player at start position 0

---
+++ src/com/snakeandladder/SnakeAndLadderUC1.java
@@ -0,0 +1,14 @@
+package com.snakeandladder;
+
+public class SnakeAndLadderUC1 {
+    private static final int START_POSITION = 0;
+
+    public static void main(String[] args) {
+        int currentPosition = START_POSITION;
+        System.out.println("Welcome to Snake and Ladder Program");
+
+        System.out.println("Player is at position: " + currentPosition);
+    }
+
+}
+
