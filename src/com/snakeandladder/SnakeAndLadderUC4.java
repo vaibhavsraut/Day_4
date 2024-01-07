f1fba051dc290876ea9039e075d8336cfa789fae
vaibhavsraut <vaibhav.raut851@gmail.com>
2024-01-07 16:22:38 +0530

    UC4: Repeat the game till winning position

---
+++ src/com/snakeandladder/SnakeAndLadderUC4.java
@@ -0,0 +1,60 @@
+package com.snakeandladder;
+import java.util.*;
+
+public class SnakeAndLadderUC4 {
+    private static final int START_POSITION = 0;
+    private static final int WINNING_POSITION = 100;
+    private static final int NO_PLAY = 0;
+    private static final int LADDER = 1;
+    private static final int SNAKE = 2;
+
+    public static void main(String[] args) {
+        int currentPosition = START_POSITION;
+        int diceRollCount = 0;
+
+        System.out.println("Welcome to Snake and Ladder Program");
+
+        while (currentPosition < WINNING_POSITION) {
+            int dieRoll = rollDie();
+            int option = checkOption();
+
+            diceRollCount++;
+
+            System.out.println("Die rolled: " + dieRoll);
+            System.out.println("Option: " + option);
+
+            currentPosition = updatePosition(currentPosition, dieRoll, option);
+
+            currentPosition = Math.max(currentPosition, START_POSITION);
+
+            System.out.println("Player is at position: " + currentPosition);
+        }
+
+        System.out.println("Player reached the exact winning position: " + WINNING_POSITION);
+        System.out.println("Number of times the dice was played to win the game: " + diceRollCount);
+    }
+
+    private static int rollDie() {
+        return (int) (Math.floor(Math.random() * 6) + 1);
+    }
+
+    private static int checkOption() {
+        return (int) (Math.floor(Math.random() * 3));
+    }
+
+    private static int updatePosition(int position, int dieRoll, int option) {
+        switch (option) {
+            case NO_PLAY:
+                break;
+            case LADDER:
+                position += dieRoll;
+                break;
+            case SNAKE:
+                position -= dieRoll;
+                position = Math.max(position, START_POSITION);
+                break;
+        }
+        position = Math.min(position, WINNING_POSITION);
+        return position;
+    }
+}
