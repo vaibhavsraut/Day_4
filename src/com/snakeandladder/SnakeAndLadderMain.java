b86b463952ac8096d0e8de0fe174b1e8ce05e663
vaibhavsraut <vaibhav.raut851@gmail.com>
2024-01-07 13:06:48 +0530

    START: Displaying Welcome message

---
+++ src/com/snakeandladder/SnakeAndLadderMain.java
@@ -0,0 +1,8 @@
+package com.snakeandladder;
+
+public class SnakeAndLadderMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder Game Program");
+    }
+}
+
